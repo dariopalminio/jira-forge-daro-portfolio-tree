cff2ad880006e237b57f58392dfc66b0e66e193f
Dario Palminio <dario.palminio@gmail.com>
2024-06-02 19:33:39 -0400

    feat(filter): version 2.1.0 added fiter by status

--- static/app/src/application/component/config/app-version.tsx
+++ static/app/src/application/component/config/app-version.tsx
@@ -6,7 +6,7 @@ const AppVersion: React.FC = () => {
 
   const getVersion = () => {
     //WARNING: Ensure that the version value is the same as that "version" value in package.json
-    const version = '2.0.2'; //HACK: temporary solution but does not follow best practices
+    const version = '2.1.0'; //HACK: temporary solution but does not follow best practices
     return version;
   }
 
